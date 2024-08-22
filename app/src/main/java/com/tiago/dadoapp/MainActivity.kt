7fa4450b3d3a66afc397ba5fe5a81082004c4c2c
Tiago Nascimento Hilario <tnhhilario@gmail.com>
2024-08-22 01:03:18 -0300

    Alterado layout do botão

--- app/src/main/java/com/tiago/dadoapp/MainActivity.kt
+++ app/src/main/java/com/tiago/dadoapp/MainActivity.kt
@@ -9,8 +9,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -21,7 +23,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.tiago.dadoapp.ui.theme.DadoAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -59,8 +63,16 @@ fun Dado(modifier: Modifier = Modifier.fillMaxSize().wrapContentSize()){
     ) {
         Image(painter = painterResource(imagemRecurso), contentDescription = "1")
         Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { resultado = (1..6).random() }) {
-            Text(stringResource(R.string.rolar))
+        OutlinedButton(
+            onClick = { resultado = (1..6).random() },
+            Modifier.width(250.dp)
+        ) {
+            Text(
+                text = stringResource(R.string.rolar).uppercase(),
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+
+            )
         }
     }
 }
